77b988f1df1c074f8b1fa8c9f2381ca7f019838c
Michał Kabionek <michalkabionek@gmail.com>
2018-04-05 18:35:54 +0200

    install redux-form

--- client/src/app.js
+++ client/src/app.js
@@ -5,9 +5,12 @@ import { createStore, applyMiddleware } from 'redux';
 import ReduxPromise from 'redux-promise';
 
 import App from './components/app';
+// import reducers from './root_reducer';
 
 const createStoreWithMiddleware = applyMiddleware(ReduxPromise)(createStore);
 
 ReactDOM.render(
+  <Provider store={createStoreWithMiddleware(reducers)}>
     <App />
+  </Provider>
   , document.querySelector('.container'));
