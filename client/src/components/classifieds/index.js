773b164ea2bb5762610c06af023c85a5bf853fba
Michał Kabionek <michalkabionek@gmail.com>
2018-04-07 12:38:23 +0200

    Add google map API

--- client/src/components/classifieds/index.js
+++ client/src/components/classifieds/index.js
@@ -1,10 +1,22 @@
 import React, { Component } from 'react';
+import GoogleMap from '../google_map';
 
 export default class ClassifiedsIndex extends Component {
+  
   render() {
+    const lon = 19.9449799;
+    const lat = 50.0646501;
     return (
-      <div>
-          <h1>Mapa Stancji</h1>
+      <div className="container-fluid classified-main">
+
+          <div className="row">
+            <div className="col-md-5">
+              <h3>Filter</h3>
+            </div>
+            <div className="col-md-7">
+              <GoogleMap lon={lon} lat={lat} />
+            </div>
+          </div>
       </div>
     );
   }
